fb34beded9a339da6ee547ba3cffc200bcd13443
Addisu <addisuhaile87@gmail.com>
2024-02-13 15:14:08 +0300

    Add UserAdmin page

--- app/core/models.py
+++ app/core/models.py
@@ -18,6 +18,7 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError("User must have an email address.")
         user = self.model(email=self.normalize_email(email), **extra_fields)
+        # Must encrypt password using set_password() that comes with BaseUserManager
         user.set_password(password)
         user.save(using=self._db)
 
