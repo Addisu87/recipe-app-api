afae861e6108e6fc4dc8b0b52d0d3db76c10afd2
Addisu <addisuhaile87@gmail.com>
2024-02-16 12:10:57 +0300

    Implement image upload and add tests

--- app/app/settings.py
+++ app/app/settings.py
@@ -144,3 +144,8 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+# To work image upload at browse able interface
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
